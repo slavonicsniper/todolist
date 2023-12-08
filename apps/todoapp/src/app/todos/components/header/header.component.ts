98030c585f9e0621facd45b8104f4b0c2fceff30
danoblinux <danoblinux@gmail.com>
2023-12-08 16:04:08 +0100

    - change components structure - add dedicated user info component into header - prepare header for profile link redirect

--- apps/todoapp/src/app/todos/components/header/header.component.ts
+++ apps/todoapp/src/app/todos/components/header/header.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
-import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-todos-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  text = '';
-
-  constructor(private todoService: TodosService) {}
-
-  changeText(event: Event) {
-    const target = event.target as HTMLInputElement;
-    this.text = target.value;
-  }
-
-  addTodo(): void {
-    this.todoService.addTodo(this.text);
-    this.text = '';
-  }
-}
+export class HeaderComponent {}
---
+++ apps/todoapp/src/app/todos/components/user-info/user-info.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { DiscordUser } from '../../types/discordUser.interface';
+
+@Component({
+  selector: 'todolist-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.scss'],
+})
+export class UserInfoComponent implements OnInit {
+  user!: DiscordUser;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.checkAuthStatus();
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+}
