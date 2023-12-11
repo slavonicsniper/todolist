f5ca3bef094d088539cb0333105f9252d94aa30c
slavonicsniper <erik.galajda.sk@gmail.com>
2023-12-12 00:43:57 +0100

    - move logout to header - load user in the todos component and pass to child components as prop - simplify css

--- apps/todoapp/src/app/todos/components/header/header.component.ts
+++ apps/todoapp/src/app/todos/components/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DiscordUser } from '../../types/discordUser.interface';
 
 @Component({
   selector: 'app-todos-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input('user') user!: DiscordUser;
+}
--- apps/todoapp/src/app/todos/components/logout/logout.component.ts
+++ apps/todoapp/src/app/todos/components/logout/logout.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss'],
 })
 export class LogoutComponent implements OnInit {
   constructor(private authService: AuthService) {}
