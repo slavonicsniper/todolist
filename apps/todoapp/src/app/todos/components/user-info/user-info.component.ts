2a6c22ba14346cb4d62abb37d66b568a5d559167
danoblinux <danoblinux@gmail.com>
2023-12-14 15:36:42 +0100

    revert temp changes

--- apps/todoapp/src/app/todos/components/user-info/user-info.component.ts
+++ apps/todoapp/src/app/todos/components/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { DiscordUser } from '../../types/discordUser.interface';
 import { DiscordService } from '../../services/discord.service';
 
