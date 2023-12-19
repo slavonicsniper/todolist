949b25e8613728039565438086c032a6e35a7eef
danoblinux <danoblinux@gmail.com>
2023-12-19 16:01:07 +0100

    fix PR comments

--- apps/todoapp/src/app/todos/components/user-info/user-info.component.ts
+++ apps/todoapp/src/app/todos/components/user-info/user-info.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { DiscordUser } from '../../types/discordUser.interface';
 import { DiscordService } from '../../services/discord.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'todolist-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent implements OnChanges {
+export class UserInfoComponent implements OnInit {
   @Input('user') user!: DiscordUser;
   userAvatar!: string;
 
-  constructor(private discordService: DiscordService) {}
+  constructor(
+    private authService: AuthService,
+    private discordService: DiscordService
+  ) {}
 
-  ngOnChanges() {
-    this.user.avatar
-      ? this.discordService
-          .getUserAvatar(this.user)
-          .then((avatar) => (this.userAvatar = avatar))
-      : this.discordService
-          .getDefaultAvatar(this.user)
-          .then((avatar) => (this.userAvatar = avatar));
+  ngOnInit() {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      this.discordService.getUserAvatar(this.user).then((avatar) => {
+        this.userAvatar = avatar;
+        if (avatar == null) {
+          this.discordService.getDefaultAvatar(this.user).then((avatar) => {
+            this.userAvatar = avatar;
+          });
+        }
+      });
+    });
   }
 }
